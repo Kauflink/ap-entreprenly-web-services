ac4971b3ea1cdec367ec0ed7435ec8191b6d65bd
Juyens <joseph.juliuscb@gmail.com>
2026-06-13 16:11:00 -0500

    feat(iam): return the created user and distinguish sign-in failures
    - Sign-up responds 201 Created with the user resource (id, email, roles)
    - Sign-in returns 404 when the email is unknown and 400 on a bad password
    - Validate sign-in/sign-up request payloads

--- src/Entreprenly.WebServices/Iam/Interfaces/Rest/Resources/SignUpResource.cs
+++ src/Entreprenly.WebServices/Iam/Interfaces/Rest/Resources/SignUpResource.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Entreprenly.WebServices.Iam.Interfaces.Rest.Resources;
 
 public record SignUpResource(
+    [property: Required] [property: EmailAddress] [property: StringLength(120)]
     string Email,
+    [property: Required] [property: StringLength(255, MinimumLength = 8)]
     string Password,
-    string? FirstName,
-    string? LastName,
-    string? Phone,
-    string? Timezone);
+    [property: StringLength(80)] string? FirstName,
+    [property: StringLength(80)] string? LastName,
+    [property: StringLength(30)] string? Phone,
+    [property: StringLength(60)] string? Timezone);
